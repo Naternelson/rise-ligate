029070af529012dde9cebfba9b2bd9468b415479
Nate <7nater.nelson@gmail.com>
2023-03-22 15:16:17 -0600

    Carousel Name changed

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,11 @@
 import { Box } from "../../components/ui/Box"
-import { Carousel, CarouselButton, Slide } from "../../components/ui/Carosel"
+import { Carousel, CarouselButton, Slide } from "../../components/ui/Carousel"
 import "./style.css"
 
 export const Dashboard = () => {
     return (
       <Box className={"center full-page"}>
-        <Carousel id={"carousel"} slideCount={3} animationTime={1000} loop>
+        <Carousel id={"carousel"} slideCount={3} animationTime={800} loop>
           <Slide index={0} className="center grow">
             <Box className={"className center"}>
               <h1>Slide 1</h1>
